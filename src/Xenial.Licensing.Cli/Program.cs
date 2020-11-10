1457be17b31f76f5ef01ffeb01fcec2f52728f24
Manuel Grundner <m.grundner@delegate.at>
2020-11-10 13:17:40 +0100

    feat: basic cli application

--- src/Xenial.Licensing.Cli/Program.cs
+++ src/Xenial.Licensing.Cli/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace Xenial.Licensing.Cli
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Xenial.Licensing.Cli;
+
+var host = CreateHostBuilder(args).Build();
+
+using var serviceScope = host.Services.CreateScope();
+
+var services = serviceScope.ServiceProvider;
+
+try
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    var myService = services.GetRequiredService<XenialLicensingApplication>();
+    return await myService.RunAsync(args);
 }
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Error Occured {0}", ex);
+    Console.ResetColor();
+    return -1;
+}
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddSingleton<XenialLicensingApplication>();
+        });
---
+++ src/Xenial.Licensing.Cli/XenialLicensingApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xenial.Licensing.Cli
+{
+    public class XenialLicensingApplication
+    {
+        public Task<int> RunAsync(string[] args)
+            => Task.FromResult(0);
+    }
+}
