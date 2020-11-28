378a99a88b528210c899831e15230b481b19e3b1
Manuel Grundner <m.grundner@delegate.at>
2020-11-28 13:53:48 +0100

    feat: try to build from github

--- build/Program.cs
+++ build/Program.cs
@@ -6,6 +6,9 @@ using Xenial.Delicious.Beer.Recipes;
 using static SimpleExec.Command;
 using static Bullseye.Targets;
 using static Xenial.Delicious.Beer.Recipes.IISRecipe;
+using static Xenial.Delicious.Beer.Json.PokeJson;
+
+const string PleaseSet = "PLEASE SET BEFORE USE";
 
 var projectName = "Xenial.Licensing";
 var sln = $"{projectName}.sln";
@@ -18,8 +21,47 @@ Target("build:npm", () => RunAsync("cmd.exe", $"/C npm run build"));
 Target("build:dotnet", DependsOn("restore:dotnet"), () => RunAsync("dotnet", $"build {sln} --no-restore"));
 Target("build", DependsOn("restore", "build:npm", "build:dotnet"));
 
-BuildAndDeployIISProject(new IISDeployOptions("Xenial.Licensing.Blazor.Server", "admin.licensing.xenial.io"), "admin");
-BuildAndDeployIISProject(new IISDeployOptions("Xenial.Licensing.Api", "api.licensing.xenial.io"), "api");
+BuildAndDeployIISProject(new IISDeployOptions("Xenial.Licensing.Blazor.Server", "admin.licensing.xenial.io")
+{
+    PrepareTask = async () =>
+    {
+        var settingsPath = "./src/Xenial.Licensing.Blazor.Server/appsettings.json";
+
+        var serverSettings = await File.ReadAllTextAsync(settingsPath);
+
+        serverSettings = serverSettings
+            .AddOrUpdateJsonValue(
+                "Authentication:Xenial:ClientSecret",
+                Environment.GetEnvironmentVariable("ADMIN_AUTHENTICATION_XENIAL_CLIENTSECRET") ?? PleaseSet
+            )
+        ;
+
+        await File.WriteAllTextAsync(settingsPath, serverSettings);
+    }
+}, "admin");
+
+BuildAndDeployIISProject(new IISDeployOptions("Xenial.Licensing.Api", "api.licensing.xenial.io")
+{
+    PrepareTask = async () =>
+    {
+        var settingsPath = "./src/Xenial.Licensing.Api/appsettings.json";
+
+        var serverSettings = await File.ReadAllTextAsync(settingsPath);
+
+        serverSettings = serverSettings
+            .AddOrUpdateJsonValue(
+                "Authentication:Xenial:ApiSecret",
+                Environment.GetEnvironmentVariable("API_AUTHENTICATION_XENIAL_APISECRET") ?? PleaseSet
+            )
+            .AddOrUpdateJsonValue(
+                "Authentication:Xenial.Swagger:ClientSecret",
+                Environment.GetEnvironmentVariable("API_AUTHENTICATION_XENIAL_SWAGGER_CLIENTSECRET") ?? PleaseSet
+            )
+        ;
+
+        await File.WriteAllTextAsync(settingsPath, serverSettings);
+    }
+}, "api");
 
 Target("publish", DependsOn("publish:admin", "publish:api"));
 
