ac912d36fcb950b1e5072b1f2450dfdd5e83993b
Manuel Grundner <m.grundner@delegate.at>
2020-11-29 11:51:28 +0100

    feat: added user info fields to trial request

--- src/Xenial.Licensing.Model/TrialRequest.cs
+++ src/Xenial.Licensing.Model/TrialRequest.cs
@@ -13,6 +13,9 @@ namespace Xenial.Licensing.Model
         private string machineKey;
         private string userId;
         private DateTime requestDate;
+        private string userCompany;
+        private string userName;
+        private string userEmail;
 
         public TrialRequest(Session session) : base(session) { }
 
@@ -35,5 +38,17 @@ namespace Xenial.Licensing.Model
         [ValueConverter(typeof(UtcDateTimeConverter))]
         [Persistent("RequestDate")]
         public DateTime RequestDate { get => requestDate; set => SetPropertyValue(ref requestDate, value); }
+
+        [Persistent("UserEmail")]
+        [Size(254)]
+        public string UserEmail { get => userEmail; set => SetPropertyValue(ref userEmail, value); }
+
+        [Persistent("UserName")]
+        [Size(254)]
+        public string UserName { get => userName; set => SetPropertyValue(ref userName, value); }
+
+        [Persistent("UserCompany")]
+        [Size(254)]
+        public string UserCompany { get => userCompany; set => SetPropertyValue(ref userCompany, value); }
     }
 }
