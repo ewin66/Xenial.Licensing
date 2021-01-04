daa3510c913731831cf0a796117131f39a85d9ac
Manuel Grundner <m.grundner@delegate.at>
2021-01-04 17:20:04 +0100

    feat: added win module

---
+++ src/Xenial.Licensing.Module.Win/Module.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using DevExpress.ExpressApp;
+
+namespace Xenial.Licensing.Module.Win
+{
+    public class LicensingWindowsFormsModule : ModuleBase
+    {
+        protected override ModuleTypeList GetRequiredModuleTypesCore()
+            => base.GetRequiredModuleTypesCore()
+                .AndModuleTypes(
+                    typeof(DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule),
+                    typeof(LicensingModule)
+                );
+
+    }
+}
