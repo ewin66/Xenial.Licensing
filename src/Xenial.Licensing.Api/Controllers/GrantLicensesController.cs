44bc9a597f6c1d1e7ade980f32b49caa889790b9
Manuel Grundner <m.grundner@delegate.at>
2021-03-09 13:34:22 +0100

    feat: update dx version

---
+++ src/Xenial.Licensing.Api/Controllers/GrantLicensesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DevExpress.Xpo;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Xenial.Licensing.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class GrantLicensesController : ControllerBase
+    {
+        private readonly UnitOfWork unitOfWork;
+
+        public GrantLicensesController(UnitOfWork unitOfWork)
+            => this.unitOfWork = unitOfWork;
+
+    }
+}
